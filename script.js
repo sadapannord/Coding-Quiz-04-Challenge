b74e8db89bc7a8d22eac261ba028fcada043f2cf
thenordplace <sadapan@gmail.com>
2022-11-14 18:21:28 -0700

    add HTML, some CSS, and some JS

---
+++ script.js
@@ -0,0 +1,30 @@
+var questions = document.querySelectorAll(".questions")
+console.log(questions)
+
+
+
+
+/* might use this from office hours
+var button = document.querySelector(".button")
+
+button.addEventListener("click",() =>{
+    button.style.display = "none";
+    setTimeout();
+    questions.style.display = "none";
+})
+
+var possibleQuestions = [
+    {
+        questionOne:"question 1",
+        answerOne:"one",
+        answerTwo: "two",
+        correctAnswer:"yep"
+    },
+    {
+        questionTwo:"guestion 2",
+        answerOne:"one",
+        answerOne: "two",
+        correctAnswer: "yep"
+    }
+] */
+
