ad144e62599905ecbfc3083b97edbb95bf2ff90e
thenordplace <sadapan@gmail.com>
2022-12-14 15:31:30 -0700

    add user inputs

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
 var questions = document.querySelectorAll(".questions")
 var questionText = document.querySelector("#questionText")
 var answersElement = document.querySelector("#answers")
-document.getElementById("questionText").style.display="none";
+document.getElementById("questionText").style.display = "none";
 console.log(questions)
-var timer; 
+var timer;
 var timerCount;
 var selectedAnswer;
 var rightAnswers = 0;
 var wrongAnswers = 0;
-var startButton= document.querySelector (".start-button")
+var startButton = document.querySelector(".start-button")
 var q1correct;
-var questionIndex =0;
+var questionIndex = 0;
 var userAnswer;
-var timerElement=document.querySelector("#timer");
+var timerElement = document.querySelector("#timer");
 var possibleQuestions = [
     {
-        question:"How do you combine arrays?",
-        answerOne:".add",
+        question: "How do you combine arrays?",
+        answerOne: ".add",
         answerTwo: ".combine",
         answerThree: ".plus",
-        correct:".concat",
+        correct: ".concat",
     },
     {
-        question:"In order to get random numbers what should you use?",
-        answerOne:"math.random()",
+        question: "In order to get random numbers what should you use?",
+        answerOne: "math.random()",
         answerTwo: "math.floor()",
         answerThree: "math.Floor()",
         correct: "Math.random()",
     },
     {
-        question:"If you wanted to change how an element looks using JS, how would you do so?",
-        answerOne:".appearance()",
+        question: "If you wanted to change how an element looks using JS, how would you do so?",
+        answerOne: ".appearance()",
         correct: ".setAttribute()",
         answerTwo: ".getAttribute()",
         answerThree: ".styleSheet()",
-        
+
     },
     {
-        question:"What does DOM stand for?",
+        question: "What does DOM stand for?",
         correct: "Document Object Model",
-        answerOne:"Document Observe Model",
+        answerOne: "Document Observe Model",
         answerTwo: "Data Object Module",
         answerThree: "Data Object Modem",
     }
 ]
-startButton.addEventListener("click",startQuiz)
+startButton.addEventListener("click", startQuiz)
 
 function startQuiz() {
     timerCount = 60;
@@ -53,77 +53,98 @@ function startQuiz() {
     startTimer();
     getQuestions();
     console.log(timerCount);
-    document.getElementById("header").style.display="none";
+    document.getElementById("header").style.display = "none";
+    var timer = document.getElementById("timer")
+    timer.innerHTML(timerCount);
+    userNameandScore();
 
-    
-  }
 
-function checkAnswer(e){
-    userAnswer=e.target.innerText
+}
+
+function checkAnswer(e) {
+    userAnswer = e.target.innerText
     console.log(userAnswer)
     //check if answer is correct
     if (userAnswer === possibleQuestions[questionIndex].correct) {
         rightAnswers++;
-        console.log(rightAnswers + "right");
-        
-    } 
+        console.log(rightAnswers + " right");
+
+    }
     else {
         wrongAnswers++;
-        timerCount -=5;
-        console.log(wrongAnswers + "wrong")
+        timerCount -= 5;
+        console.log(wrongAnswers + " wrong")
     }
     //increment question by 1
     questionIndex++;
     //check if there are more questions to display, show next question if there is
-    if(possibleQuestions.length > questionIndex) {
+    if (possibleQuestions.length > questionIndex) {
         getQuestions()
     }
     else {
         clearInterval(timer);
-        var wrongScore=document.createElement("p");
-        wrongScore.innerHTML="You got "+ wrongAnswers+ " questions wrong";
+        var wrongScore = document.createElement("p");
+        wrongScore.innerHTML = "You got " + wrongAnswers + " questions wrong";
         document.body.appendChild(wrongScore);
-        var correctScore=document.createElement("p");
-        correctScore.innerHTML="You got "+ rightAnswers+ " questions correct";
+        var correctScore = document.createElement("p");
+        correctScore.innerHTML = "You got " + rightAnswers + " questions correct";
         document.body.appendChild(correctScore);
-        document.getElementById("questionText").style.display="none";
-        document.getElementById("answers").style.display="none";
-        document.getElementById("timer").style.display="none";
+        var finalScore = document.createElement("p");
+        finalScore.innerHTML = "Your score was " + rightAnswers;
+        document.body.appendChild(finalScore);
+        document.getElementById("questionText").style.display = "none";
+        document.getElementById("answers").style.display = "none";
+        document.getElementById("timer").style.display = "none";
+        var form = document.getElementById("form");
+        var submit = document.getElementById("submit");
+        form.style.display = "flex";
+        submit.addEventListener("submit",userInput())
+        function userInput() {
+            var userName = document.getElementById("userName").value;
+            var score = document.getElementById("score").value;
+            console.log(userName, score)
+        }
+        if (rightAnswers > localStorage.getItem("highscore")) {
+            localStorage.setItem("highscore", rightAnswers)
+            localStorage.setItem("username", userName)
+            localStorage.setItem("score", score)
+    }
+
         /*if (rightAnswers>=localStorage.getItem("highscore")){
             localStorage.setItem("highscore",rightAnswers)}*/
-        userNameandScore()
+        // userNameandScore()
     }
-    
- }
+
+}
 
 
 function startTimer() {
     // Sets timer
-    timer = setInterval(function() { //timer has been declared but not set
+    timer = setInterval(function () { //timer has been declared but not set
         timerElement.textContent = timerCount;
         timerCount--; //timerCount was set to 30 in line 28, now is 29 because -1
-      if (timerCount <= 0) {        
-          clearInterval(timer);    
-      }
+        if (timerCount <= 0) {
+            clearInterval(timer);
+        }
     }, 1000);
-  }
+}
 
-function getQuestions(){
-    document.getElementById("questionText").style.display="flex"
-    questionText.textContent= possibleQuestions[questionIndex].question;
-    var answer1=document.createElement("button");
-    answer1.addEventListener("click",checkAnswer);
-    answer1.textContent=possibleQuestions[questionIndex].answerOne;
-    var answer2=document.createElement("button");
+function getQuestions() {
+    document.getElementById("questionText").style.display = "flex"
+    questionText.textContent = possibleQuestions[questionIndex].question;
+    var answer1 = document.createElement("button");
+    answer1.addEventListener("click", checkAnswer);
+    answer1.textContent = possibleQuestions[questionIndex].answerOne;
+    var answer2 = document.createElement("button");
     answer2.addEventListener("click", checkAnswer);
-    answer2.textContent=possibleQuestions[questionIndex].answerTwo;
-    var answer3=document.createElement("button");
+    answer2.textContent = possibleQuestions[questionIndex].answerTwo;
+    var answer3 = document.createElement("button");
     answer3.addEventListener("click", checkAnswer);
-    answer3.textContent=possibleQuestions[questionIndex].answerThree;
-    var correctAnswer=document.createElement("button");
+    answer3.textContent = possibleQuestions[questionIndex].answerThree;
+    var correctAnswer = document.createElement("button");
     correctAnswer.addEventListener("click", checkAnswer);
-    correctAnswer.textContent=possibleQuestions[questionIndex].correct;
-    answersElement.innerHTML="";
+    correctAnswer.textContent = possibleQuestions[questionIndex].correct;
+    answersElement.innerHTML = "";
     answersElement.append(answer1, answer2, answer3, correctAnswer);
 
 }
@@ -131,16 +152,8 @@ console.log(rightAnswers)
 console.log(wrongAnswers)
 
 
-function userNameandScore(){
-    var userName=document.getElementById("userName")
-    var score=document.getElementById("score")
-    if (rightAnswers>localStorage.getItem("highscore")){
-        localStorage.setItem("highscore",rightAnswers)
-    }
-    userName=window.prompt("Username")
-    score=window.prompt("Score")
-    console.log(userName)
-} 
+function userNameandScore() {
+}
 //startQuiz();
 
 /*function checkCorrect(){
